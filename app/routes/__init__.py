6c6cc4373957341d84c44adcc964726e52c143c6
220alexandre <conquer.aliance@gmail.com>
2024-06-18 21:53:42 -0300

    att erros

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,29 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from redis import Redis
 from apscheduler.schedulers.background import BackgroundScheduler
-from config import Config
 from app.utils.token_reset import reset_token_usage
 from app.extensions import db, login_manager, csrf, limiter
 from app.models import User
-from app.monitor import get_system_usage, system_usage
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('config.Config')
 
     db.init_app(app)
     Migrate(app, db)
     csrf.init_app(app)
     limiter.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'auth_routes.login'
+    login_manager.login_view = 'auth.login'
 
     from app.routes import admin_routes, auth_routes, main_routes, stripe_routes
-    app.register_blueprint(admin_routes.bp, name='admin_routes')
-    app.register_blueprint(auth_routes.bp, name='auth_routes')
-    app.register_blueprint(main_routes.bp, name='main_routes')
-    app.register_blueprint(stripe_routes.bp, name='stripe_routes')
-
-    app.add_url_rule('/system_usage', 'system_usage', system_usage)
+    app.register_blueprint(admin_routes.bp, url_prefix='/admin')
+    app.register_blueprint(auth_routes.bp, url_prefix='/auth')
+    app.register_blueprint(main_routes.bp)
+    app.register_blueprint(stripe_routes.bp)
 
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=reset_token_usage, trigger='cron', day=1, hour=0, minute=0, id='reset_token_usage', replace_existing=True)
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import db, User
-from datetime import datetime
 from app.extensions import limiter
 
-bp = Blueprint('auth_routes', __name__)
+bp = Blueprint('auth', __name__)
 
 @bp.route('/login', methods=['GET', 'POST'])
 @limiter.limit("5 per minute")
@@ -39,4 +38,4 @@ def register():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth_routes.login'))
+    return redirect(url_for('auth.login'))
