4ba10177e557f2cf965d985e3ede655c4fcd8d75
220alexandre <conquer.aliance@gmail.com>
2024-06-18 21:59:40 -0300

    corrige datetime

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import db, User
 from app.extensions import limiter
+from datetime import datetime
 
 bp = Blueprint('auth', __name__)
 
